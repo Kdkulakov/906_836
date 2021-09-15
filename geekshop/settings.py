09a55639224cb3d1bc111229d946ad61e43c1867
kulakov <kdkulakov@gmail.com>
2021-09-15 15:36:43 +0300

    fix auto_field

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'geekshop.urls'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
