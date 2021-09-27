9bd23b03088dc6b00a5bfdf18aff2799b3fabb7c
kulakov <kdkulakov@gmail.com>
2021-09-27 21:30:17 +0300

    add settings to prod

--- geekshop/settings_prod.py
+++ geekshop/settings_prod.py
@@ -94,10 +94,12 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'geekshop',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
