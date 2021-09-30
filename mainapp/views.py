21408a7f84297c4429c29954f04d80288e8c5a38
kulakov <kdkulakov@gmail.com>
2021-09-30 22:14:06 +0300

    add hv testing

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,7 +13,7 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request,category_id=None,page=1):
+def products(request, category_id=None, page=1):
 
     context = {
         'title': 'GeeKshop',
@@ -28,12 +28,12 @@ def products(request,category_id=None,page=1):
 
     }
     if category_id:
-        products = Product.objects.filter(category_id = category_id).order_by('name')
+        products = Product.objects.filter(category_id=category_id).order_by('name')
     else:
         products = Product.objects.all().order_by('name')
     paginator = Paginator(products, per_page=3)
     products_paginator = paginator.page(page)
-    context.update({'product':products_paginator})
+    context.update({'product': products_paginator})
     return render(request, 'mainapp/products.html', context)
 
 
